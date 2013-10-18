6d3678a905ce070e3f4946cc192de2c7b3f9bdc7
Hackbright Student <user@chromebox08.(none)>
2013-10-17 17:46:46 -0700

    website with buttons! Adds new book to list view

--- jsClassesandObjects/readinglist.js
+++ jsClassesandObjects/readinglist.js
@@ -8,11 +8,8 @@ var Booklist = function() {
         this.bookShelf = this.unreadBooks.concat(this.readBooks);
         this.booksRead = this.readBooks.length;
         this.booksNotRead = this.unreadBooks.length;
-        this.currentBook = this.unreadBooks[0];
-        this.nextBook = this.unreadBooks[1];
-        this.previousBook = this.readBooks[this.readBooks.length-1];
+        this.updateLists();
         return console.log(newBook._title + " added successfully.");
-
     };
 
     this.finishCurrentBook = function() {
@@ -20,14 +17,17 @@ var Booklist = function() {
         this.currentBook.read = true;
         this.readBooks.push(this.currentBook);
         this.currentBook = this.unreadBooks.shift();
-        this.currentBook = this.unreadBooks[0];
-        this.nextBook = this.unreadBooks[1];
-        this.previousBook = this.readBooks[this.readBooks.length-1];
+        this.updateLists();
         return console.log("Finished " + this.previousBook._title + '.');
     };
 
-
+    this.updateLists = function() {
+        this.currentBook = this.unreadBooks[0];
+        this.nextBook = this.unreadBooks[1];
+        this.previousBook = this.readBooks[this.readBooks.length-1];
+    };
 };
+
 
 var Book = function(_title, genre, author, read){
     this._title = _title;
@@ -35,16 +35,12 @@ var Book = function(_title, genre, author, read){
     this.author = author;
     this.read = false;
     this.readDate = null;
-
 };
 
 
 var book1 = new Book("The Magical Pope Dragon", "Fantasy", "Nick A", false);
-
 var book2 = new Book("The Magical Pope Unicorn", "Fantasy", "Nick A", false);
-
 var book3 = new Book("The Mostly-Magical Pope Cat", "Fantasy", "Nick A", false);
-
 var book4 = new Book("The Not-Really-Magical-At-All Cat", "Non-Fiction", "Nick A", false);
 
 var nicksBookList = new Booklist();
